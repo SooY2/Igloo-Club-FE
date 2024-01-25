27e7023dfd70c2abe5c7dd97a21c784e99057073
sooy <ssy67366736@gmail.com>
2024-01-25 13:51:48 +0900

    feat:router설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Login from './login';
 import AuthKakao from './login/AuthKakao';
+import Landing from './landing/Landing';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Landing />,
+  },
+  {
+    path: 'login',
     element: <Login />,
   },
   {
