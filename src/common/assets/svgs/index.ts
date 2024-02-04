89984754f9ca035902e9454c9c4e44d026f27c54
gkswldnjs78 <jw4319han@naver.com>
2024-02-05 02:08:01 +0900

    feat: 마이페이지 뷰 작업

---
+++ src/common/assets/svgs/index.ts
@@ -0,0 +1,19 @@
+import HomeBtn000 from '../assets/svgs/home-black.svg?react';
+import ChatBtn000 from '../assets/svgs/chat-black.svg?react';
+import HeartBtn000 from '../assets/svgs/heart-black.svg?react';
+import PeopleBtn000 from '../assets/svgs/people-black.svg?react';
+import HomeBtnGRAY from '../assets/svgs/home-white.svg?react';
+import ChatBtnGRAY from '../assets/svgs/chat-white.svg?react';
+import HeartBtnGRAY from '../assets/svgs/heart-white.svg?react';
+import PeopleBtnGRAY from '../assets/svgs/people-white.svg?react';
+
+export {
+  HomeBtn000,
+  ChatBtn000,
+  HeartBtn000,
+  PeopleBtn000,
+  HomeBtnGRAY,
+  ChatBtnGRAY,
+  HeartBtnGRAY,
+  PeopleBtnGRAY,
+};
