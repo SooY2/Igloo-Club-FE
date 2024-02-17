dd4cccd606ce575874858aafb126ed4597908004
sooy <ssy67366736@gmail.com>
2024-02-17 14:47:17 +0900

    feat: 약관동의 링크연결

--- src/register/funnelPages/RegisterMemberAgreeFunnel.tsx
+++ src/register/funnelPages/RegisterMemberAgreeFunnel.tsx
@@ -5,21 +5,7 @@ import * as St from '../styles/registerStyles';
 import { css } from '@emotion/react';
 import { useEffect, useState } from 'react';
 import RegisterBtn from '../components/RegisterBtn';
-
-const 약관동의리스트 = [
-  {
-    title: '서비스 이용 약관 (필수)',
-    url: 'https://tremendous-magnolia-67f.notion.site/6211374a9b844d47bbc8762d022300cd?pvs=74',
-  },
-  {
-    title: '개인정보 수집 및 이용 동의 (필수)',
-    url: 'https://tremendous-magnolia-67f.notion.site/5d564021ae194985a5a298f3e77077c1?pvs=4',
-  },
-  {
-    title: '마케팅 정보 수신 동의 (선택)',
-    url: 'https://tremendous-magnolia-67f.notion.site/a8e95dfdbac2499f84225b55f7537720?pvs=4',
-  },
-];
+import { 약관동의리스트 } from '../../common/constants/memberAgreeConstants';
 
 /** 체크박스 컴포넌트입니다 */
 const Check = ({
@@ -105,7 +91,7 @@ const 약관동의 = ({
                         isAgree={agree[idx]}
                         onChange={() => setAgreeAtIndex(idx, !agree[idx])}
                       />
-                      <p>{item.title}</p>
+                      <p>{`${item.title} (${item.option})`}</p>
                     </StListTitle>
                     <ArrowRight
                       onClick={() => window.open(item.url, '_blank')}
