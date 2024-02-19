89fbd3f88cfefe74a36167380b7eb90e6ed57afa
gkswldnjs78 <jw4319han@naver.com>
2024-02-19 11:44:14 +0900

    feat: 지도 렌더링 오류

--- src/common/components/Map.tsx
+++ src/common/components/Map.tsx
@@ -30,7 +30,7 @@ const Map = ({ matchData, setIsClickedMarker }: MapProps) => {
 
     if (window.kakao && window.kakao.maps) {
       mapScript.onload = () => {
-        setTimeout(onLoadKakaoMap, 1000);
+        setTimeout(onLoadKakaoMap, 100);
       };
     }
 
@@ -120,7 +120,7 @@ const Map = ({ matchData, setIsClickedMarker }: MapProps) => {
         }
       };
     };
-  }, [matchData, clickedMarker, setIsClickedMarker]);
+  }, [matchData, clickedMarker, setClickedMarker, setIsClickedMarker]);
 
   return <div id="map" css={MapContainer}></div>;
 };
