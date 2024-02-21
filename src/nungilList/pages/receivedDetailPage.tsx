53e2d9ee7d77287241836aefc2336c9fa1166824
sooy <ssy67366736@gmail.com>
2024-02-21 11:32:15 +0900

    fix: 스크롤

--- src/nungilList/pages/receivedDetailPage.tsx
+++ src/nungilList/pages/receivedDetailPage.tsx
@@ -33,7 +33,9 @@ const Container = css`
   flex-direction: column;
   justify-content: space-between;
   width: 100%;
+  height: 100%;
   padding-top: 1.5rem;
+  overflow-y: scroll;
 `;
 
 const SendBtn = css`
