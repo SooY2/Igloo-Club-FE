da92d8a99f10b30b502cb3596969c47df93fde07
gkswldnjs78 <jw4319han@naver.com>
2024-03-12 13:23:15 +0900

    fix: 히어로 이미지 로고 추가

---
+++ src/login/assets/images/0_index.ts
@@ -0,0 +1,6 @@
+import Carousel1 from './Carousel1.png';
+import Carousel2 from './Carousel2.png';
+import Carousel3 from './Carousel3.png';
+import Carousel4 from './Carousel4.png';
+
+export { Carousel1, Carousel2, Carousel3, Carousel4 };
