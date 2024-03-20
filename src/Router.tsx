9df37e2a64ede68a0accbaa64b8e0e9fcc780b45
sooy <ssy67366736@gmail.com>
2024-03-20 13:27:21 +0900

    fix: 사전신청 풀기

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,17 +3,17 @@ import Login from './login';
 import AuthKakao from './login/AuthKakao';
 import Landing from './landing/Landing';
 import Register from './register/RegisterPage';
-// // import MainPage from './main/pages/mainPage';
-// import DetailPage from './main/pages/DetailPage';
-// import FinishMatch from './main/pages/finishMatch';
-// import NungilList from './nungilList/pages/nungilList';
-// import ReceivedDetailPage from './nungilList/pages/receivedDetailPage';
-// import SendDetailPage from './nungilList/pages/sendDetailPage';
-// import MyPage from './mypage/pages/myPage';
-// import EditProfilePage from './mypage/pages/editProfilePage';
-// import Chat from './chat/Chat';
-// import ChatRoom from './chat/components/chatRoom/ChatRoom';
-import 사전신청완료 from './common/components/RegisterAdvanceApplication';
+import MainPage from './main/pages/mainPage';
+import DetailPage from './main/pages/DetailPage';
+import FinishMatch from './main/pages/finishMatch';
+import NungilList from './nungilList/pages/nungilList';
+import ReceivedDetailPage from './nungilList/pages/receivedDetailPage';
+import SendDetailPage from './nungilList/pages/sendDetailPage';
+import MyPage from './mypage/pages/myPage';
+import EditProfilePage from './mypage/pages/editProfilePage';
+import Chat from './chat/Chat';
+import ChatRoom from './chat/components/chatRoom/ChatRoom';
+// import 사전신청완료 from './common/components/RegisterAdvanceApplication';
 
 const router = createBrowserRouter([
   {
@@ -30,20 +30,20 @@ const router = createBrowserRouter([
       },
       {
         path: '/main-page',
-        element: <사전신청완료 />,
+        element: <MainPage />,
       },
-      // {
-      //   path: 'detailpage/:nungilId',
-      //   element: <DetailPage />,
-      // },
-      // { path: 'finishmatch/:nungilId', element: <FinishMatch /> },
-      // { path: 'nungillist', element: <NungilList /> },
-      // { path: 'receiveddetailpage/:nungilId', element: <ReceivedDetailPage /> },
-      // { path: 'senddetailpage/:nungilId', element: <SendDetailPage /> },
-      // { path: 'mypage', element: <MyPage /> },
-      // { path: 'editprofilepage', element: <EditProfilePage /> },
-      // { path: 'chat', element: <Chat /> },
-      // { path: 'chat/:chatRoomId', element: <ChatRoom /> },
+      {
+        path: 'detailpage/:nungilId',
+        element: <DetailPage />,
+      },
+      { path: 'finishmatch/:nungilId', element: <FinishMatch /> },
+      { path: 'nungillist', element: <NungilList /> },
+      { path: 'receiveddetailpage/:nungilId', element: <ReceivedDetailPage /> },
+      { path: 'senddetailpage/:nungilId', element: <SendDetailPage /> },
+      { path: 'mypage', element: <MyPage /> },
+      { path: 'editprofilepage', element: <EditProfilePage /> },
+      { path: 'chat', element: <Chat /> },
+      { path: 'chat/:chatRoomId', element: <ChatRoom /> },
     ],
   },
   {
